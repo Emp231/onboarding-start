62d05da096eb0e1745ca01977789bfd6b2705d18
Samanyu Shrivastava <samshri06@gmail.com>
2025-09-27 19:34:05 -0400

    changed stuff

--- src/project.v
+++ src/project.v
@@ -36,7 +36,19 @@ module tt_um_uwasic_samanyu_shrivastava (
     .out({uio_out, uo_out})
   );
 
-  // List all unused inputs to prevent warnings
+  spi_peripheral spi_peripheral_inst (
+    .clk(clk), 
+    .rst_n(rst_n),
+    .nCS_in(ui_in[2]),   
+    .COPI_in(ui_in[1]),  
+    .SCLK_in(ui_in[0]),  
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle)
+  );
+
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
