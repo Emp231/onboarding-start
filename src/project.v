ae319f24f45a3005a8c6af66c5d65dadd03518b6
Samanyu Shrivastava <samshri06@gmail.com>
2025-09-28 20:14:33 -0400

    testing

--- src/project.v
+++ src/project.v
@@ -39,9 +39,9 @@ module tt_um_uwasic_samanyu_shrivastava (
   spi_peripheral spi_peripheral_inst (
     .clk(clk), 
     .rst_n(rst_n),
-    .nCS(ui_in[2]),        
-    .COPI(ui_in[1]),     
-    .SCLK(ui_in[0]),      
+    .nCS_in(ui_in[2]),        
+    .COPI_in(ui_in[1]),     
+    .SCLK_in(ui_in[0]),      
     .en_reg_out_7_0(en_reg_out_7_0),
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
